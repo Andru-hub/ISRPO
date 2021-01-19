13aecffdfc5c0cfb88479d0864ee247aa2e81ac3
Andru-hub <67545441+Andru-hub@users.noreply.github.com>
2021-01-19 10:53:22 +0200

    Add files via upload

--- ПЗ02_Vladimirov/src/com/company/Main.java
+++ ПЗ02_Vladimirov/src/com/company/Main.java
@@ -4,22 +4,58 @@ public class Main {
 
     public static void main(String[] args) {
 
-        class Variables {
+        // char to String
+        char ch = 'S' ;
 
-            public void main (String args []) {
+        // c использованием класса Character
+        String charToString = Character.toString(ch);
+        System.out.println(charToString);
 
-                double a = 3;
+        // с использованием операции добавления класса String
+        String str = "" + ch;
+        System.out.println(str);
 
-                double b = 4;
+        //с использованием массива
+        String fromChar = new String(new char[] { ch });
+        System.out.println(fromChar);
 
-                double c;
+        // с использованием метода valueOf класса String
+        String valueOfchar = String.valueOf(ch);
+        System.out.println(valueOfchar);
 
-                c = Math.sqrt (a* a + b* b);
 
-                System.out.println ("c = "+ c);
+        //    char to int
+        char ch1 = '9';
 
-            }
+        // c использованием метода getNumericValue
+        // класса Character
+        int i1 = Character.getNumericValue(ch1);
+        System.out.println(i1);
+
+        // c использованием метода digit класса Character
+        int i2 = Character.digit(ch1,10);
+        System.out.println(i2);
+
+        // int to long
+        int iq = 2015;
+        long l = (long) (iq);
+        System.out.println(l);
+
+        //  int to float
+        int iw = 2015;
+        float f = (float) (iw);
+        System.out.println(f);
+
+        //    long to int
+        long le = 214748364;
+        int i = (int) le;
+        System.out.println(i);
+
+        //   double to int
+        double d = 3.14;
+        int ir = (int) d;
+        System.out.println(ir);
         }
 
     }
-}
+
